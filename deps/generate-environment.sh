1bc501b1ce6e4a0691ab5614d705c86d9c215d50
Andrea Telatin <15690844+telatin@users.noreply.github.com>
2022-09-02 10:29:45 +0100

    Updated container

--- deps/generate-environment.sh
+++ deps/generate-environment.sh
@@ -1,5 +1,5 @@
 mamba create -n cleanup -y -c conda-forge -c bioconda \
-  nextflow pigz "seqfu>=1.9" "kraken2=2.1.0" "krakentools>=1.2" bracken  "multiqc>1.9" \
+  nextflow pigz "seqfu>=1.14.0" "kraken2=2.1.0" "krakentools>=1.2" bracken  "multiqc>1.9" \
   "samtools>=1.12" bwa kaiju fastp \
   "megahit" minia prodigal quast eggnog-mapper \
   bbmap
